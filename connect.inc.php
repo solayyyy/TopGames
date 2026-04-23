c02efb5a0885e6bb85f946d46f2dd2b405fbb853
L1ilouuu <lilougrimaud.13@gmail.com>
2026-04-23 21:23:52 +0200

    Page login ajout fichier auth.php / login.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,9 +1,15 @@
 <?php
-
+/*
 $host = "mysql03.univ-lyon2.fr"; 
 $login = "php_lepascal"; 
 $password = "l3idgTDfwQYHUBT7055sR-ZLK";
 $dbname = $login;
+*/
 
-
+//Db local pour tests 
+$host = "localhost"; 
+$port = 3307;
+$login = "root"; 
+$password = "";
+$dbname = "topgames";
 ?>
